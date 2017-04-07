c176ec9a69fc8c9cf7f6a03a8f82d3c732436c62
jabou356 <jason.bouffard@mail.mcgill.ca>
2017-04-07 14:16:33 -0400

    BatchTRC

---
+++ Batch_TRC_RPT.m
@@ -0,0 +1,20 @@
+%Script developed by Jason Bouffard in order to generate .trc files as an
+%input to OpenSim from .klo files generated during the RPT in the OBEL lab
+%at McGill. Use function sg_KLOtoTRC developped by Shaheen Ghayourmanesh and functions
+%GenericPathRPT and subjectPathRPT developed by Jason Bouffard
+
+GenericPathRPT
+
+d=dir(Path.DataPath);
+j=0;
+for i=1:length(d)
+    if strcmp(d(i).name{1:7},'Subject')==1
+        j=j+1;
+        subjectID(j)=str2num(d(i).name{8:end});
+    end
+end
+
+subjectID=sort(subjectID);
+
+for isujet=
+
