6a6f9bd4f451427f3f6c6a49f83204764cfe2a85
jabou356 <jason.bouffard@mail.mcgill.ca>
2017-04-07 09:46:17 -0400

    ModigyGenPath

---
+++ GenericPath.m
@@ -0,0 +1,8 @@
+Path.ProjectPath=uigetdir('C:','Go get the project folder');
+
+
+%% Setup common paths
+Path.OpensimSetupJB=[Path.ProjectPath '\OpenSimSetUpFiles\'];
+Path.OpensimGenericModel=[Path.OpensimSetupJB,'StandfordVA_wmarkers.osim'];
+Path.OpensimGenericScale=[Path.OpensimSetupJB,'Conf_scaling.xml'];
+Path.OpensimGenericIK=[Path.OpensimSetupJB,'Conf_IK.xml'];
