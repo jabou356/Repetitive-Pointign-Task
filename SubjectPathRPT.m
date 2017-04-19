291e9cd6d34a7c6367953098d5c613869fe285c1
Jason Bouffard <jason.bouffard@mail.mcgill.ca>
2017-04-19 16:49:51 -0400

    Change \ for /

--- SubjectPathRPT.m
+++ SubjectPathRPT.m
@@ -1,27 +1,27 @@
 %% Chemin des fichiers
     % Dossier du sujet
-    Path.SubjectPath  = [Path.DataPath 'Subject' num2str(subjectID(isubject)) '\'];
+    Path.SubjectPath  = [Path.DataPath 'Subject' num2str(subjectID(isubject)) '/'];
     % Dossier du modèle pour le sujet
     Path.ScaledModel  = [Path.SubjectPath 'StandfordVA_Hiram' num2str(subjectID(isubject)) '.osim'];
     Path.ScaledAdjustedModel  = [Path.SubjectPath 'StandfordVA_Hiram' num2str(subjectID(isubject)) 'Adjusted.osim'];
 
     % Dossier des data
-    Path.KLOimportPath = [Path.SubjectPath 'inputKLO\'];
+    Path.KLOimportPath = [Path.SubjectPath 'inputKLO/'];
     % Dossiers d'exportation
-    Path.exportPath = [Path.SubjectPath 'Result\'];
+    Path.exportPath = [Path.SubjectPath 'Result/'];
     
     if isdir(Path.exportPath)==0
         mkdir(Path.exportPath);
     end
     
-    Path.TRCpath=[Path.SubjectPath,'TRC\'];
+    Path.TRCpath=[Path.SubjectPath,'TRC/'];
     if isdir(Path.TRCpath)==0
         mkdir(Path.TRCpath);
     end
     
-    Path.IKpath=[Path.SubjectPath,'IK\'];
-    Path.IKresultpath=[Path.IKpath,'result\'];
-    Path.IKsetuppath=[Path.IKpath,'setup\'];
+    Path.IKpath=[Path.SubjectPath,'IK/'];
+    Path.IKresultpath=[Path.IKpath,'result/'];
+    Path.IKsetuppath=[Path.IKpath,'setup/'];
     if isdir(Path.IKpath)==0
         mkdir(Path.IKpath);
         mkdir(Path.IKresultpath);
