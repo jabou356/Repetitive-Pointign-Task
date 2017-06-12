67fa7959a3ccb7c4b13c8e4040773c6dcef5fcf0
jabou356 <jason.bouffard@mail.mcgill.ca>
2017-06-12 16:13:44 -0400

    Code to partition IK from OSIM

---
+++ Batch_PartitionOSIM.m
@@ -0,0 +1,37 @@
+clear clc
+
+GenericPathRPT
+
+d=dir(Path.DataPath);
+j=0;
+for i=1:length(d)
+    if length(d(i).name)>6
+        if strcmp(d(i).name(1:7),'Subject')==1
+            j=j+1;
+            subjectID(j)=str2num(d(i).name(8:end));
+        end
+    end
+end
+
+
+
+for isubject=[1:3 7 9:length(subjectID) ]
+    disp(['Processing subject #' num2str(subjectID(isubject)) ' (' num2str(isubject) ' out of ' num2str(length(subjectID)) ')'])
+    
+    SubjectPathRPT;
+    
+    Klofiles=dir([Path.exportPath '*.klo']);
+
+    
+    for itrial = 1 : length(Klofiles)
+        %Import .klo file
+        KloName=Klofiles(itrial).name;
+        load([Path.exportPath KloName], '-mat', 'data');
+        
+        PartitionOSIM
+        
+        save([Path.exportPath KloName],'data')
+        
+        clear data
+    
+end;
---
+++ PartitionOSIM.m
@@ -0,0 +1,52 @@
+SignalEndpoint={'RIDX'};
+
+VideoLength=length(fieldnames(data.VideoData));
+
+for ichan=1:VideoLength
+isRIDX(ichan)=strcmp(data.VideoData.(['channel' num2str(ichan)]).label,SignalEndpoint);
+end
+    
+EndpointData=data.VideoData.(['channel' num2str(find(isRIDX))]).xdata;
+
+%% Filter joint angles 15Hz lowpass
+[b, a]=butter(2, 15/50, 'low');
+for ichan=1:length(fieldnames(data.OSIMDoF))
+    fdata.OSIMDoF.(['channel' num2str(ichan)]).data=...
+        filtfilt(b,a,data.OSIMDoF.(['channel' num2str(ichan)]).data);
+end
+
+
+kfwd=0;
+kbwd=0;
+for imvt=1:length(data.PartData.Xvideo)-1
+    
+    start=data.PartData.Xvideo(imvt);
+    finish=data.PartData.Xvideo(imvt+1);
+    isfwd = EndpointData(finish) - EndpointData(start) > 0;
+    
+    % Variables for interpolation  
+    x=1:(finish-start);
+   
+    
+    if isfwd
+        kfwd=kfwd+1;
+        for ichan=1:length(fieldnames(data.OSIMDoF))
+         y=fdata.OSIMDoF.(['channel' num2str(ichan)]).data(start:finish-1);    
+         channame=data.OSIMDoF.(['channel' num2str(ichan)]).label;       
+         data.Forward.(channame)(:,kfwd)=interp1(x,y,1:(length(x)-1)/99:length(x));
+        end
+            
+    else
+        kbwd=kbwd+1;
+        for ichan=1:length(fieldnames(data.OSIMDoF))
+         y=fdata.OSIMDoF.(['channel' num2str(ichan)]).data(start:finish-1);
+         channame=data.OSIMDoF.(['channel' num2str(ichan)]).label;       
+         data.Backward.(channame)(:,kbwd)=interp1(x,y,1:(length(x)-1)/99:length(x));
+        end
+        
+    end
+            
+    
+end
+
+   
