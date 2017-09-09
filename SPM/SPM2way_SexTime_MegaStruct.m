99152593922c7940a89e4cd5a48285a077c064f9
Unknown <jason.bouffard@mail.mcgill.ca>
2017-09-08 21:07:49 -0400

    SPM code, not sure used

--- SPM/SPM2way_SexTime_MegaStruct.m
+++ SPM/SPM2way_SexTime_MegaStruct.m
@@ -1,63 +1,29 @@
 %Determine what signal we compare
 clear;clc;close all;
 
-signal={'ShoulderPlane'};
+signal={'Shoulderplane'};
 statistic={'Mean'};
-project={'Kathryn'};
+%project={'Kathryn'};
 
-% data=xlsread('C:\Users\Jason\OneDrive - McGill University\RepetitivePointingTask_forJason\GroupData\Analyses\MegaDatabase.xlsx'...
-% ,'RawDatabase1D','K3:HB529');
-% 
-% [~,signalxl,~]=xlsread('C:\Users\Jason\OneDrive - McGill University\RepetitivePointingTask_forJason\GroupData\Analyses\MegaDatabase.xlsx'...
-% ,'RawDatabase1D','I3:I529');
-% 
-% [~,statisticxl,~]=xlsread('C:\Users\Jason\OneDrive - McGill University\RepetitivePointingTask_forJason\GroupData\Analyses\MegaDatabase.xlsx'...
-% ,'RawDatabase1D','J3:J529');
-% 
-% [~,projectxl,~]=xlsread('C:\Users\Jason\OneDrive - McGill University\RepetitivePointingTask_forJason\GroupData\Analyses\MegaDatabase.xlsx'...
-% ,'RawDatabase1D','D3:D529');
-% 
-% subjectxl=xlsread('C:\Users\Jason\OneDrive - McGill University\RepetitivePointingTask_forJason\GroupData\Analyses\MegaDatabase.xlsx'...
-% ,'RawDatabase1D','B3:B529');
-% 
-% timexl=xlsread('C:\Users\Jason\OneDrive - McGill University\RepetitivePointingTask_forJason\GroupData\Analyses\MegaDatabase.xlsx'...
-% ,'RawDatabase1D','C3:C529');
-% 
-% sexxl=xlsread('C:\Users\Jason\OneDrive - McGill University\RepetitivePointingTask_forJason\GroupData\Analyses\MegaDatabase.xlsx'...
-% ,'RawDatabase1D','E3:E529');
+GenericPathRPT
 
+load([Path.JBAnalyse, 'GroupData0D.mat']);
 
+Y=[Megadatabase(strcmp(signal{1},[Megadatabase.Signal]) & ...
+    strcmp(statistic{1},[Megadatabase.Stat]) & ...
+    ~isnan([Megadatabase.ROMFwd])).ROMFwd];
 
-% Y=data(strcmp(signalxl,signal)&strcmp(statisticxl,statistic)&strcmp(projectxl,project),:);
-% A=sexxl(strcmp(signalxl,signal)&strcmp(statisticxl,statistic)&strcmp(projectxl,project));
-% B=timexl(strcmp(signalxl,signal)&strcmp(statisticxl,statistic)&strcmp(projectxl,project));
-% SUBJ=subjectxl(strcmp(signalxl,signal)&strcmp(statisticxl,statistic)&strcmp(projectxl,project));
+A=[Megadatabase(strcmp(signal{1},[Megadatabase.Signal]) & ...
+    strcmp(statistic{1},[Megadatabase.Stat]) & ...
+    ~isnan([Megadatabase.ROMFwd])).Sex];
 
-Y=data(strcmp(signalxl,signal)&strcmp(statisticxl,statistic),:);
-A=sexxl(strcmp(signalxl,signal)&strcmp(statisticxl,statistic));
-B=timexl(strcmp(signalxl,signal)&strcmp(statisticxl,statistic));
-SUBJ=subjectxl(strcmp(signalxl,signal)&strcmp(statisticxl,statistic));
+B=[Megadatabase(strcmp(signal{1},[Megadatabase.Signal]) & ...
+    strcmp(statistic{1},[Megadatabase.Stat]) & ...
+    ~isnan([Megadatabase.ROMFwd])).Time];
 
-
-valid=find(~isnan(Y(:,1)));
-Y=Y(valid,1:100);
-A=A(valid);
-B=B(valid);
-SUBJ=SUBJ(valid);
-
-
-males=unique(SUBJ(A==1));
-females=unique(SUBJ(A==2));
-    
-npergroup=min([length(males),length(females)]);
-
-
-A=[A(ismember(SUBJ,males(1:npergroup)));A(ismember(SUBJ,females(1:npergroup)))];
-B=[B(ismember(SUBJ,males(1:npergroup)));B(ismember(SUBJ,females(1:npergroup)))];
-Y=[Y(ismember(SUBJ,males(1:npergroup)),:);Y(ismember(SUBJ,females(1:npergroup)),:)];
-SUBJ=[SUBJ(ismember(SUBJ,males(1:npergroup)));SUBJ(ismember(SUBJ,females(1:npergroup)))];
-
-
+SUBJ=[Megadatabase(strcmp(signal{1},[Megadatabase.Signal]) & ...
+    strcmp(statistic{1},[Megadatabase.Stat]) & ...
+    ~isnan([Megadatabase.ROMFwd])).SubjectID];
 
 
 %(1) Conduct non-parametric test:
