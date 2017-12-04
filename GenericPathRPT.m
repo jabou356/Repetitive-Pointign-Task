db261235c948d389af80a772fd1967bbc175548e
Unknown <jason.bouffard@mail.mcgill.ca>
2017-12-04 10:23:05 -0500

    Clean and comment Path Files

--- GenericPathRPT.m
+++ GenericPathRPT.m
@@ -1,12 +1,22 @@
+% Go get the Parent project folder: In our case it is the
+% RepetitivePointingTask_forJason folder in the OneDrive
 Path.ProjectPath=uigetdir('C:','Go get the project folder');
+
+% Indicate data from which project you want to analyze (e.g. 'Jason',
+% 'Asha', etc.) DO NOT FORGET THE QUOTES
 projet=input('Which project are you working on?');
-machinetype=menu('Are you working on a PC or a MAC?', 'PC', 'MAC');
+
+
 %% Setup common paths
-if machinetype == 1
+
     
-    Path.DataPath=[Path.ProjectPath '\Data\', projet, '\'];
-    Path.OpensimSetupJB=[Path.ProjectPath '\OpenSimSetUpFiles\'];
+    % single subjects Data Path
+    Path.DataPath=[Path.ProjectPath '/Data/', projet, '/'];
     
+    % OpenSim Setup files (Generic models, scale config, IK config, etc.)
+    Path.OpensimSetupJB=[Path.ProjectPath '/OpenSimSetUpFiles/'];
+    
+    % If the project is Kathryn, take her model (T12 instead of T10)
     if strcmp(projet,'Kathryn')
         
         Path.OpensimGenericModel=[Path.OpensimSetupJB,'Humerothoracic_wscapula - Kathryn.osim'];
@@ -17,30 +27,19 @@ if machinetype == 1
         
     end
     
+    %  OpenSim scale config
     Path.OpensimGenericScale=[Path.OpensimSetupJB,'Conf_scaling - ' projet '.xml'];
-    Path.OpensimGenericIK=[Path.OpensimSetupJB,'Conf_IK.xml'];
     
-    Path.OpensimGenericIKtrunk=[Path.OpensimSetupJB,'Conf_IK_trunk.xml'];
-    Path.OpensimGenericIKtrunkmot=[Path.OpensimSetupJB,'Conf_IK_wmot.xml'];
+    % OpenSim IK config 
+    Path.OpensimGenericIK=[Path.OpensimSetupJB,'Conf_IK.xml']; %(generic, not used anymore) 
+    Path.OpensimGenericIKtrunk=[Path.OpensimSetupJB,'Conf_IK_trunk.xml']; %(Compute Trunk DoFs first)
+    Path.OpensimGenericIKtrunkmot=[Path.OpensimSetupJB,'Conf_IK_wmot.xml']; %(Input Trunk DoFs to compute other DoFs)
 
-    
+    % OpenSim Body kinematics config, not used
     Path.OpensimGenericBK=[Path.OpensimSetupJB,'Conf_BK.xml'];
     
-    Path.GroupPath=[Path.ProjectPath '\GroupData\'];
-    Path.GroupDataPath=[Path.GroupPath 'MegaDatabase\'];
-    Path.JBAnalyse=[Path.GroupPath 'Analysis\Jason\'];
-    
-elseif machinetype == 2
-    
-    Path.DataPath=[Path.ProjectPath '/Data/', projet, '/'];
-    Path.OpensimSetupJB=[Path.ProjectPath '/OpenSimSetUpFiles/'];
-    Path.OpensimGenericModel=[Path.OpensimSetupJB,'Humerothoracic_wscapula.osim'];
-    Path.OpensimGenericScale=[Path.OpensimSetupJB,'Conf_scaling.xml'];
-    Path.OpensimGenericIK=[Path.OpensimSetupJB,'Conf_IK.xml'];
-    Path.OpensimGenericBK=[Path.OpensimSetupJB,'Conf_BK.xml'];
-    
-    
-    Path.GroupPath=[Path.ProjectPath '/GroupData/'];
-    Path.GroupDataPath=[Path.GroupPath 'MegaDatabase/'];
-    Path.JBAnalyse=[Path.GroupPath 'Analysis/Jason/'];    
-end
+    % Group Paths
+    Path.GroupPath=[Path.ProjectPath '/GroupData/']; % Parent group path
+    Path.GroupDataPath=[Path.GroupPath 'MegaDatabase/']; % Path containing the .txt database
+    Path.JBAnalyse=[Path.GroupPath 'Analysis/Jason/']; % Path where Jason do data analysis (stats. etc.)
+   
--- SubjectPathRPT.m
+++ SubjectPathRPT.m
@@ -1,78 +1,36 @@
-%% Chemin des fichiers
+%% Subject specific paths
 
-if machinetype == 1
-    % Dossier du sujet
-    Path.SubjectPath  = [Path.DataPath 'Subject' num2str(subjectID(isubject)) '\'];
-    % Dossier du modèle pour le sujet
+    % Parent subject path
+    Path.SubjectPath  = [Path.DataPath 'Subject' num2str(subjectID(isubject)) '/'];
+    
+    % Subject specific scaled and adjusted (technical markers placed) models
     Path.ScaledModel  = [Path.SubjectPath 'StandfordVA_' projet num2str(subjectID(isubject)) '.osim'];
     Path.ScaledAdjustedModel  = [Path.SubjectPath 'StandfordVA_' projet num2str(subjectID(isubject)) 'Adjusted.osim'];
 
-    % Dossier des data
-    Path.KLOimportPath = [Path.SubjectPath 'inputKLO\'];
-    % Dossiers d'exportation
-    
-    Path.RotateKLOPath = [Path.SubjectPath 'rotateKLO\'];
-    if isdir(Path.RotateKLOPath)==0
-        mkdir(Path.RotateKLOPath);
-    end
-    
-    Path.exportPath = [Path.SubjectPath 'Result\'];
-    
-    if isdir(Path.exportPath)==0
-        mkdir(Path.exportPath);
-    end
-    
-    Path.TRCpath=[Path.SubjectPath,'TRC\'];
-    if isdir(Path.TRCpath)==0
-        mkdir(Path.TRCpath);
-    end
-    
-    Path.IKpath=[Path.SubjectPath,'IK\'];
-    Path.IKresultpath=[Path.IKpath,'result\'];
-    Path.IKsetuppath=[Path.IKpath,'setup\'];
-    if isdir(Path.IKpath)==0
-        mkdir(Path.IKpath);
-        mkdir(Path.IKresultpath);
-        mkdir(Path.IKsetuppath);
-    end
-    
-    Path.BKpath=[Path.SubjectPath,'BodyKinematics\'];
-    Path.BKresultpath=[Path.BKpath,'result\'];
-    Path.BKsetuppath=[Path.BKpath,'setup\'];
-    if isdir(Path.BKpath)==0
-        mkdir(Path.BKpath);
-        mkdir(Path.BKresultpath);
-        mkdir(Path.BKsetuppath);
-    end
-    
-elseif machinetype == 2
-    
-     % Dossier du sujet
-    Path.SubjectPath  = [Path.DataPath 'Subject' num2str(subjectID(isubject)) '/'];
-    % Dossier du modèle pour le sujet
-    Path.ScaledModel  = [Path.SubjectPath 'StandfordVA_Hiram' num2str(subjectID(isubject)) '.osim'];
-    Path.ScaledAdjustedModel  = [Path.SubjectPath 'StandfordVA_Hiram' num2str(subjectID(isubject)) 'Adjusted.osim'];
-
-    % Dossier des data
+    % Path toward inputKLO files
     Path.KLOimportPath = [Path.SubjectPath 'inputKLO/'];
-    % Dossiers d'exportation
     
+    % Path toward Rotated KLOfiles (x: vector between PTRG and DTRG, y: up, z: cross(x,y)    
     Path.RotateKLOPath = [Path.SubjectPath 'rotateKLO/'];
     if isdir(Path.RotateKLOPath)==0
         mkdir(Path.RotateKLOPath);
     end
     
+    % Path toward the result folder (including partitionned, IK, etc.)
     Path.exportPath = [Path.SubjectPath 'Result/'];
     
     if isdir(Path.exportPath)==0
         mkdir(Path.exportPath);
     end
     
+    % Path toward TRC files (Rotated marker position used to input in
+    % OpenSim
     Path.TRCpath=[Path.SubjectPath,'TRC/'];
     if isdir(Path.TRCpath)==0
         mkdir(Path.TRCpath);
     end
     
+    % Path toward IK results (Output from OpenSIM)
     Path.IKpath=[Path.SubjectPath,'IK/'];
     Path.IKresultpath=[Path.IKpath,'result/'];
     Path.IKsetuppath=[Path.IKpath,'setup/'];
@@ -82,6 +40,7 @@ elseif machinetype == 2
         mkdir(Path.IKsetuppath);
     end
     
+    % Path toward Body kinematics (output from OpenSim, not used)
     Path.BKpath=[Path.SubjectPath,'BodyKinematics/'];
     Path.BKresultpath=[Path.BKpath,'result/'];
     Path.BKsetuppath=[Path.BKpath,'setup/'];
@@ -91,6 +50,6 @@ elseif machinetype == 2
         mkdir(Path.BKsetuppath);
     end
     
-end
+
     
    
