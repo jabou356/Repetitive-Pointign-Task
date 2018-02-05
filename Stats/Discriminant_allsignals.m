06c794830dfe1f2d0f5af141e415f0c165bd440c
Unknown <jason.bouffard@mail.mcgill.ca>
2018-02-05 16:24:54 -0500

    Discirminant validity report

--- Stats/Discriminant_allsignals.m
+++ Stats/Discriminant_allsignals.m
@@ -1,43 +1,111 @@
-clear J OptimalThreshold
-%% For each signal, compute Discriminant validity statistics for each threshold value and find optimal threshold (ROC area)
-for isignal = length(DataBaseRPT.CAssignAll):-1:1
-    
+clear 
+close all
+
+load('ai_indices.mat');
+load('DatabaseRPT.mat');
+%% Compute confusion analysis for each variable (ROC analysis) 
+for isignal = 1:length(DataBaseRPT.CAssignAll)
+% Generate confusion table for each threshold value, from min(X) to max(X)   
 [Xvalue(:,isignal), TruePositive(:,isignal), TrueNegative(:,isignal), FalsePositive(:,isignal), FalseNegative(:,isignal)] = Discriminant(DataBaseRPT.AllX(isignal,train+1),DataBaseRPT.Y(train+1));
 
-% Find the optimal cutoff point based on 
-J(:,isignal) = TruePositive(:,isignal)./(TruePositive(:,isignal)+FalseNegative(:,isignal))+TrueNegative(:,isignal)./(TrueNegative(:,isignal)+FalsePositive(:,isignal))-1;
+% Compute Sensitivity and Specificity at each threshold value
+Sensitivity(:,isignal)= TruePositive(:,isignal)./(TruePositive(:,isignal)+FalseNegative(:,isignal));
+Specificity(:,isignal)= TrueNegative(:,isignal)./(TrueNegative(:,isignal)+FalsePositive(:,isignal));
+
+% Find the optimal cutoff point based on Youden's J statistic
+J(:,isignal) = Sensitivity(:,isignal) + Specificity(:,isignal) - 1 ;
 OptimalID(isignal)=find(J(:,isignal)==max(J(:,isignal)),1,'first');
 OptimalThreshold(isignal)=Xvalue(OptimalID(isignal),isignal);
-Sensitivity(isignal)= TruePositive(OptimalID(isignal),isignal)/(TruePositive(OptimalID(isignal),isignal)+FalseNegative(OptimalID(isignal),isignal));
-Specificity(isignal)= TrueNegative(OptimalID(isignal),isignal)/(TrueNegative(OptimalID(isignal),isignal)+FalsePositive(OptimalID(isignal),isignal));
+
+optSpec(isignal) = Specificity(OptimalID(isignal),isignal);
+optSens(isignal) = Sensitivity(OptimalID(isignal),isignal);
+
+% AUC = int (y*dx) here dx is 1/100;
+ROCauc(isignal) = abs(trapz(1-Specificity(:,isignal),Sensitivity(:,isignal)));
 
 end
 
-%% Group the signals with a SRM > 0.8 in Bouffard et al 2018 by adding their Z scores
+%% Compute confusion analysis (ROC analysis) for a compound variable (i.e. summing Z scores of variables with SRM > 0.8 in Bouffard et al 2018)
+% Compute Z score for the variables with SRM > 0.8
 Z=zscore(DataBaseRPT.BestX,[],2);
+
+% Revert the z scores for variables whose values decrease with fatigue
 revert=find(mean(DataBaseRPT.BestX(:,DataBaseRPT.Y==1),2)>mean(DataBaseRPT.BestX(:,DataBaseRPT.Y==2),2));
 Z(revert,:)=Z(revert,:)*-1;
+
+% Generate confusion table for each threshold value, from min(X) to max(X) 
 [Xvalue(:,isignal+1), TruePositive(:,isignal+1), TrueNegative(:,isignal+1), FalsePositive(:,isignal+1), FalseNegative(:,isignal+1)] = Discriminant(sum(Z(:,train+1),1),DataBaseRPT.Y(train+1));
 DataBaseRPT.CAssignAll{isignal+1}='BestXCombined';
 
-J(:,isignal+1) = TruePositive(:,isignal+1)./(TruePositive(:,isignal+1)+FalseNegative(:,isignal+1))+TrueNegative(:,isignal+1)./(TrueNegative(:,isignal+1)+FalsePositive(:,isignal+1))-1;
+% Compute Sensitivity and Specificity at each threshold value
+Sensitivity(:,isignal+1)= TruePositive(:,isignal+1)./(TruePositive(:,isignal+1)+FalseNegative(:,isignal+1));
+Specificity(:,isignal+1)= TrueNegative(:,isignal+1)./(TrueNegative(:,isignal+1)+FalsePositive(:,isignal+1));
+
+% Find the optimal cutoff point based on Youden's J statistic
+J(:,isignal+1) = Sensitivity(:,isignal+1) + Specificity(:,isignal+1) - 1 ;
 OptimalID(isignal+1)=find(J(:,isignal+1)==max(J(:,isignal+1)),1,'first');
 OptimalThreshold(isignal+1)=Xvalue(OptimalID(isignal+1),isignal+1);
-Sensitivity(isignal+1)= TruePositive(OptimalID(isignal+1),isignal+1)/(TruePositive(OptimalID(isignal+1),isignal+1)+FalseNegative(OptimalID(isignal+1),isignal+1));
-Specificity(isignal+1)= TrueNegative(OptimalID(isignal+1),isignal+1)/(TrueNegative(OptimalID(isignal+1),isignal+1)+FalsePositive(OptimalID(isignal+1),isignal+1));
-%% For each signal
-close all
 
-g=gramm('x',(FalsePositive./(FalsePositive+TrueNegative))','y',(TruePositive./(TruePositive+FalseNegative))');%, 'label', round(data.proportion))
-g.fig(DataBaseRPT.CAssignAll);
-g.geom_line();
-g.geom_label('Color','auto');
-g.draw;
+optSpec(isignal+1) = Specificity(OptimalID(isignal+1),isignal+1);
+optSens(isignal+1) = Sensitivity(OptimalID(isignal+1),isignal+1);
 
-figure
-g=gramm('x',1-Sensitivity,'y',Specificity,'label', DataBaseRPT.CAssignAll,'color', max(J));
+% AUC = int (x*y*dx) here dx is 1/100;
+ROCauc(isignal+1) = abs(trapz(1-Specificity(:,isignal+1),Sensitivity(:,isignal+1)));
+
+%% Figure 1: Position of the optimal cutoff point for each variable on a ROC curve (Train Dataset)
+
+g=gramm('x',1-optSpec,'y',optSens,'label', DataBaseRPT.CAssignAll,'color', max(J));
 g.geom_point();
 g.geom_label('Color','k');
 g.axe_property('YLim',[0 1],'XLim', [0 1]); 
-g.set_point_options('base_size',12)
+g.set_point_options('base_size',12);
+g.geom_abline();
+g.set_names('x', 'False Positive rate; 1-specificity', 'y', 'True positive rate. sensitivity', 'color', 'Youden'' s J');
 g.draw;
+
+[~, rankJ] = sort(max(J(:,:)), 'descend');
+for isignal = 1: size(Sensitivity,2)
+    disp([DataBaseRPT.CAssignAll{rankJ(isignal)}, ': Sensitivity = ', num2str(Sensitivity(OptimalID(rankJ(isignal)),rankJ(isignal))), '; Specifcity = ', ...
+        num2str(Specificity(OptimalID(rankJ(isignal)),rankJ(isignal))), ...
+        '; Optimal Threshold = ', num2str(OptimalThreshold(rankJ(isignal))), '; Youden''s J = ', num2str(max(J(:,rankJ(isignal))))])
+end
+
+%% Figure 2: Actual performance of each variables on Test Dataset
+
+% Generate confusion table for each variable at opimal threshold value using test Data   
+[testTruePositive, testTrueNegative, testFalsePositive, testFalseNegative] = DiscriminantTest([DataBaseRPT.AllX(:,test+1);sum(Z(:,test+1),1)],DataBaseRPT.Y(test+1),OptimalThreshold);
+
+% Compute sensitivity and specificity for the optimal cutoff point using test Data
+testSensitivity = testTruePositive./(testTruePositive+testFalseNegative);
+testSpecificity = testTrueNegative./(testTrueNegative+testFalsePositive);
+testJ = testSensitivity + testSpecificity - 1;
+
+% Generate report
+figure
+g=gramm('x',1-testSpecificity,'y',testSensitivity,'label', DataBaseRPT.CAssignAll,'color', testJ);
+g.geom_point();
+g.geom_label('Color','k');
+g.axe_property('YLim',[0 1],'XLim', [0 1]); 
+g.set_point_options('base_size',12);
+g.geom_abline();
+g.set_names('x', 'False Positive rate; 1-specificity', 'y', 'True positive rate. sensitivity', 'color', 'Youden'' s J');
+g.draw;
+
+for isignal = 1: size(Sensitivity,2)
+    disp([DataBaseRPT.CAssignAll{rankJ(isignal)}, ': Sensitivity = ', num2str(testSensitivity(rankJ(isignal))), '; Specifcity = ', num2str(testSpecificity(rankJ(isignal))), ...
+        '; Optimal Threshold = ', num2str(OptimalThreshold(rankJ(isignal))), '; Youden''s J = ', num2str(testJ(rankJ(isignal)))])
+end
+
+%% Figures: ROC curve for each variable (TrainDataset)
+for isignal = 1: size(Sensitivity,2)
+    disp([DataBaseRPT.CAssignAll{rankJ(isignal)}, ': ROC auc = ', num2str(ROCauc(rankJ(isignal)))])
+end
+
+figure 
+g=gramm('x',(1-Specificity)','y',Sensitivity');
+g.fig(DataBaseRPT.CAssignAll);
+g.geom_line();
+g.geom_abline();
+g.set_names('x', 'False Positive rate; 1-specificity', 'y', 'True positive rate. sensitivity');
+g.draw;
+
